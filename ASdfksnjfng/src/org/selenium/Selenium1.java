fff8ceebbee13a687e87be05cfeb42f3032d5990
Praveen <praveenparker10@gmail.com>
2021-03-17 19:01:41 -0700

    commit

---
+++ ASdfksnjfng/src/org/selenium/Selenium1.java
@@ -0,0 +1,12 @@
+package org.selenium;
+
+public class Selenium1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("praveen");
+		System.out.println("naveen");
+		System.out.println("janani");
+	}
+
+}
